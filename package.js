521623225d08878ee43918539e56ea4dd2547501
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2015-05-25 22:32:21 +0900

    version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'new3rs:twitter',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'a synchronous wrapper of NPM twitter',
   // URL to the Git repository containing the source code for this package.
